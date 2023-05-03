29f7afca8aa105ba2f2402403e66cafffd2fcf15
Lichitek <71435373+Lichitek@users.noreply.github.com>
2023-05-03 18:05:37 +0300

    exp_gold_fix

--- New Slavia/Assets/_Scripts/Systems/Health/EnemyStats.cs
+++ New Slavia/Assets/_Scripts/Systems/Health/EnemyStats.cs
@@ -8,17 +8,21 @@ public class EnemyStats : CharacterStats
     [SerializeField] private float _gold;
 
     [SerializeField] private GameObject expOrbPrefab;
-    [SerializeField] private GameObject goldOrbPrefab;
+    //[SerializeField] private MoneySystem playerMoney;
+    //[SerializeField] private GameObject goldOrbPrefab;
 
     protected override void Die()
     {
         base.Die();
         Debug.Log(transform.name + " умер");
-        for (int i = 0; i < _experience; i += 5)
-        {
-            Instantiate(expOrbPrefab, transform.position, Quaternion.identity);
-            Instantiate(goldOrbPrefab, transform.position, Quaternion.identity);
-        }
+        Instantiate(expOrbPrefab, transform.position, Quaternion.identity);
+        MoneySystem playerMoney = FindObjectOfType<MoneySystem>();
+        playerMoney.TakeGold();
+        //for (int i = 0; i < _experience; i += 5)
+        //{
+        //    Instantiate(expOrbPrefab, transform.position, Quaternion.identity);
+        //    Instantiate(goldOrbPrefab, transform.position, Quaternion.identity);
+        //}
         Destroy(gameObject);
     }
 }
